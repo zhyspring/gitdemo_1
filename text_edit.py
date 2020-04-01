936c545a0a95eec0ac53248e99cfda78fd3cd749
ZHY323 <zhjyhy@126.com>
2020-04-01 18:13:27 +0800

    initial update

---
+++ text_edit.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  9 20:02:06 2019
+
+@author: zhy
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  9 10:27:51 2019
+
+@author: zhy
+"""
+import re         #正则表达书去除空格和标点符号,数字
+punctuation = '.!,;:?"\''+"("+")"+"0"+"1"+"2"+"3"+"4"+"5"+"6"+"7"+"8"+"9"
+
+import operator
+
+import matplotlib.pyplot as plt
+
+
+
+#加入GUI
+from tkinter import *
